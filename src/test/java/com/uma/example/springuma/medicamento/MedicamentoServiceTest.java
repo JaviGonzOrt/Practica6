7b19fff00dc14bb0b63954b6db7ed0a4d867c5ef
rosalia garcia jimenez <rosaliagj2@uma.es>
2025-12-17 10:29:50 +0100

    Estoy ya con los test de service

--- src/test/java/com/uma/example/springuma/medicamento/MedicamentoServiceTest.java
+++ src/test/java/com/uma/example/springuma/medicamento/MedicamentoServiceTest.java
@@ -1,14 +1,19 @@
 package com.uma.example.springuma.medicamento;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.uma.example.springuma.model.Medicamento;
 import com.uma.example.springuma.model.MedicamentoService;
 import com.uma.example.springuma.model.RepositoryMedicamento;
-
-import jakarta.inject.Inject;
 
 public class MedicamentoServiceTest {
 
@@ -30,5 +35,22 @@ public class MedicamentoServiceTest {
         verify(repositoryMedicamento).findAll();
     }
     
+    @Test
+    @DisplayName("Comprueba que devuelve mas de un medicamento")
+    public void getAllMedicamentos_ListaNoVacia() {
+        // Arrage
+        Medicamento medicamento1 = new Medicamento();
+        Medicamento medicamento2 = new Medicamento();
+        when(repositoryMedicamento.findAll()).thenReturn(Arrays.asList(medicamento1, medicamento2));
+        // Act
+        List<Medicamento> result = medicamentoService.getAllMedicamentos();
+        int sizeObtained = result.size();
+        // Assert
+        assertEquals(2, sizeObtained);
+        verify(repositoryMedicamento).findAll();
+    }
+
+    
+
     
 }
